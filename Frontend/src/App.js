711dc6baa184bc33566e92f02369433075c8a5f9
leandroab07 <leandroab07@gmail.com>
2022-05-18 17:27:06 -0300

    Inicia o Frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div>
+      <h1>Organizador de Tarefas</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <p>Meu form</p>
+      <input placeholder="Tarefa"/>
+      <button>Add</button>
+    </div>
+  )
+}
